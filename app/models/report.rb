61c36686a43186f8b16dc59beef11f81dcfda5ce
RcNgtm <rika.nagatomo243@gmail.com>
2022-10-08 13:24:00 +0900

    ベーシック認証実装

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :detail
   has_many :stocks
+
+  validates :product, presence: true
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
   belongs_to :report
   belongs_to :user
+
+  validates :movedate, presence: true
+  validates :num, presence: true, numericality: { only_integer: true }
 end
