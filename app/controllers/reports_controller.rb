b1171534dc1ce8724c05b7ee05281b6bf0d0f3cc
RcNgtm <rika.nagatomo243@gmail.com>
2022-10-16 23:57:35 +0900

    ユーザー登録への遷移
    ログイン時でも登録画面へ遷移できる

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,13 @@
+class ReportsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+  end
+
+  def new
+  end
+
+  def crete
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root 'stocks#index'
   resources :stocks
-  devise_for :users
+  resources :reports, only: [:index, :new, :create]
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+    devise_scope :user do
+    get 'users/index', to: 'users/registrations#index'
+    get 'users/destroy', to: 'users/sessions#destroy'
+  end 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
