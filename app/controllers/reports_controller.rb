4c8d2dbef0af7d7ad233c6ea199cd6a2f0ffcdd9
RcNgtm <rika.nagatomo243@gmail.com>
2022-10-20 01:20:14 +0900

    帳票情報登録

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,24 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
   end
 
   def new
+    @report = Report.new
+    @data = Detail.all
   end
 
-  def crete
+  def create
+    Report.create!(stock_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def stock_params
+    params.require(:report).permit(:product, :detail_id)
   end
 
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,8 +1,12 @@
 module StocksHelper
   def product_color(report)
-    if report.product == "医療保険"
-    return 'bg_pink'  
-    else report.product == "定期保険"
+    if report.product == "緩和医療"
+    return 'bg_purple'  
+    elsif report.product == "緩和定期"
+    return 'bg_brown'
+    elsif report.product == "医療"
+    return 'bg_pink'
+    else report.product == "定期"
     return 'bg_green'
     end
   end   
